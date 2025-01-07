c8c3d2a923d12fa1a728725fb51f0c2019740431
bhargavnama <bhargavnama143@gmail.com>
2025-01-07 20:20:35 +0530

    Fix line endings for package-lock.json, package.json, App.jsx, and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home.jsx'
-import CreateBook from './pages/createBook.jsx'
-import DeleteBook from './pages/deleteBook.jsx'
-import EditBook from './pages/editBook.jsx'
-import ShowBook from './pages/showBook.jsx'
+import { CreateBook } from './pages/createBook.jsx'
+import { DeleteBook } from './pages/deleteBook.jsx'
+import { EditBook } from './pages/editBook.jsx'
+import { ShowBook } from './pages/showBook.jsx'
 
 export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/books/create' element={<createBook />}/>
-      <Route path='/books/details/:id' element={<showBook />}/>
-      <Route path='/books/edit/:id' element={<editBook />}/>
-      <Route path='/books/delete/:id' element={<deleteBook />}/>
+      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/details/:id' element={<ShowBook />}/>
+      <Route path='/books/edit/:id' element={<EditBook />}/>
+      <Route path='/books/delete/:id' element={<DeleteBook />}/>
     </Routes>
   )
 }
--- frontend/src/pages/showBook.jsx
+++ frontend/src/pages/showBook.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import { Spinner } from '../components/Spinner'
 
-export const showBook = () => {
+export const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((res) => {
+      setBook(res.data);
+      setLoading(false);
+    })
+    .catch(err => {
+      console.log(err);
+      setLoading(false);
+    })
+  }, [])
   return (
-    <div>showBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {
+        (loading)?(
+          <Spinner />
+        ):(
+          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+            <div className="my-4">
+              <span className="text-xl mr-4 text-blue-500">Id</span>
+              <span>{book._id}</span>
+            </div>
+            <div className="my-4">
+              <span className="text-xl mr-4 text-blue-500">Title</span>
+              <span>{book.title}</span>
+            </div>
+            <div className="my-4">
+              <span className="text-xl mr-4 text-blue-500">Author</span>
+              <span>{book.author}</span>
+            </div>
+            <div className="my-4">
+              <span className="text-xl mr-4 text-blue-500">Publish Year</span>
+              <span>{book.publishYear}</span>
+            </div>
+          </div>
+        )
+      }
+    </div>
   )
 }
